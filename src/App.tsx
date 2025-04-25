88fba1410e3505bce47fe1efc186ac2f8e7c3c1f
Vactor0911 <vactor0911@gmail.com>
2025-04-25 13:31:03 +0900

    스타일 변경

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Header from "./components/Header";
 const App = () => {
   return (
     <>
-      <CssBaseline />
+      <CssBaseline /> 
       <ThemeProvider theme={theme}>
         <Header />
         <BrowserRouter>
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,18 +1,8 @@
-import { Button, Stack } from "@mui/material";
-import FontTest from "../components/FontTest";
+import { Stack } from "@mui/material";
 
 const Main = () => {
   return (
-    <Stack gap={1}>
-      <Button variant="contained" color="primary">
-        Primary Button
-      </Button>
-
-      <Button variant="contained" color="secondary">
-        Primary Button
-      </Button>
-
-      <FontTest />
+    <Stack>
     </Stack>
   );
 };
