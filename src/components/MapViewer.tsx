c3f0d3e5b4aebff7c1ffd5fc8ac4db8444ac2d56
Vactor0911 <vactor0911@gmail.com>
2025-05-05 03:19:00 +0900

    교내 지도 뷰어 구현

---
+++ src/components/MapViewer.tsx
@@ -0,0 +1,51 @@
+import { ImageOverlay, MapContainer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { LatLngBoundsExpression, CRS } from "leaflet";
+import MapImage from "/images/map.png";
+
+const bounds: LatLngBoundsExpression = [
+  [0, 0],
+  [3840, 2160],
+];
+
+const maxBounds: LatLngBoundsExpression = [
+  [0, 100],
+  [3840, 2060],
+];
+
+const MapViewer = () => {
+  return (
+    <MapContainer
+      crs={CRS.Simple}
+      bounds={bounds}
+      maxBounds={maxBounds}
+      maxBoundsViscosity={1.0}
+      maxZoom={1.5}
+      scrollWheelZoom={true}
+      css={{
+        height: "100%",
+        "& .leaflet-control-container .leaflet-top.leaflet-left": {
+          bottom: "22px",
+          right: "4px",
+          left: "initial",
+          top: "initial",
+        },
+        "& .leaflet-control-attribution > a:first-child, & .leaflet-control-attribution > span":
+          {
+            display: "none",
+          },
+      }}
+    >
+      <ImageOverlay
+        url={MapImage}
+        bounds={bounds}
+        attribution='<a href="https://www.mokwon.ac.kr/view/html/sub05/0501.html" style="display: flex; align-items: center; gap: 4px;">
+          <img src="/logo.png" alt="logo" height="14px" />
+          목원대학교 3D 캠퍼스맵
+        </a>'
+      />
+    </MapContainer>
+  );
+};
+
+export default MapViewer;
