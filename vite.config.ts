be0bf5e4a7ac8ff18d0914d81fc8631b393ddcd1
Vactor0911 <vactor0911@gmail.com>
2025-05-05 03:18:33 +0900

    emotion 환경설정

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxImportSource: "@emotion/react",
+    }),
+  ],
   server: {
     allowedHosts: ["0.tcp.jp.ngrok.io"],
   },
