842494593525d8231b60c8b913d70995e5367c7c
Vactor0911 <vactor0911@gmail.com>
2025-05-05 14:16:13 +0900

    교내 지도 뷰어 척도 수정 및 버튼 추가

--- src/components/MapViewer.tsx
+++ src/components/MapViewer.tsx
@@ -2,8 +2,10 @@ import { ImageOverlay, MapContainer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { LatLngBoundsExpression, CRS } from "leaflet";
 import MapImage from "/images/map.png";
-import { useMediaQuery } from "@mui/material";
+import { Button, useMediaQuery } from "@mui/material";
 import { theme } from "../theme";
+import { useCallback, useState } from "react";
+import MyLocationRoundedIcon from "@mui/icons-material/MyLocationRounded";
 
 const MapViewer = () => {
   const bounds: LatLngBoundsExpression = useMediaQuery(
@@ -11,24 +13,33 @@ const MapViewer = () => {
   )
     ? [
         [0, 0],
-        [3840, 2160],
+        [1920, 1080],
       ]
     : [
         [0, 0],
-        [1920, 1080],
+        [960, 540],
       ];
 
   const maxBounds: LatLngBoundsExpression = useMediaQuery(
     theme.breakpoints.up("sm")
   )
     ? [
-        [0, 100],
-        [3840, 2060],
+        [0, -500],
+        [1920, 1580],
       ]
     : [
-        [0, 50],
-        [1920, 930],
+        [0, 0],
+        [960, 540],
       ];
+
+  // 내 위치 버튼 클릭
+  const [map, setMap] = useState<L.Map | null>(null);
+  const handleMyLocationClick = useCallback(() => {
+    const center: [number, number] = [500, 500];
+    const zoom = 0;
+
+    map?.setView(center, zoom);
+  }, [map]);
 
   return (
     <MapContainer
@@ -36,13 +47,14 @@ const MapViewer = () => {
       bounds={bounds}
       maxBounds={maxBounds}
       maxBoundsViscosity={1.0}
-      maxZoom={1.5}
+      maxZoom={2.5}
       scrollWheelZoom={true}
+      ref={setMap}
       css={{
         height: "100%",
         "& .leaflet-control-container .leaflet-top.leaflet-left": {
           bottom: "22px",
-          right: "4px",
+          right: "6px",
           left: "initial",
           top: "initial",
         },
@@ -57,9 +69,28 @@ const MapViewer = () => {
         bounds={bounds}
         attribution='<a href="https://www.mokwon.ac.kr/view/html/sub05/0501.html" style="display: flex; align-items: center; gap: 4px;">
           <img src="/logo.png" alt="logo" height="14px" />
-          목원대학교 3D 캠퍼스맵
+          목원대학교 목원뷰
         </a>'
       />
+
+      {/* 내 위치 버튼 */}
+      <Button
+        variant="contained"
+        color="secondary"
+        sx={{
+          padding: "8px",
+          minWidth: "0",
+          borderRadius: "50%",
+          color: "white",
+          position: "absolute",
+          bottom: "90px",
+          right: "5px",
+          zIndex: 1000,
+        }}
+        onClick={handleMyLocationClick}
+      >
+        <MyLocationRoundedIcon />
+      </Button>
     </MapContainer>
   );
 };
