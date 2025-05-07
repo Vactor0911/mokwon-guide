70b74771c92cc217005b5e4709c59ee2b3c31136
L-HITS <leechan753@gmail.com>
2025-05-08 02:24:45 +0900

    검색 기록 로컬스토리지 추가

--- src/states/index.ts
+++ src/states/index.ts
@@ -1,6 +1,17 @@
 import { atom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
+import { encryptedStorage } from "../utils";
 
+
+// 좌측 상단 메뉴 아이콘 클릭 시 열리는 Drawer 상태 관리 아톰
 export const isDrawerOpenAtom = atom(false);
 
+// 우측 상단 검색 아이콘 클릭 시 열리는 Drawer 상태 관리 아톰
 export const isSearchDrawerOpenAtom = atom(false);
 
+// 검색 기록 - 암호화된 형태로 localStorage에 저장
+export const searchHistoryAtom = atomWithStorage<string[]>(
+  'mokwon-search-history', 
+  [], 
+  encryptedStorage
+);
