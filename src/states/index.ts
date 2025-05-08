7a0467b1b7f540f170846c5a8c32f77b406ce387
Vactor0911 <vactor0911@gmail.com>
2025-05-08 21:20:33 +0900

    전역 상태 추가

--- src/states/index.ts
+++ src/states/index.ts
@@ -1,17 +1,23 @@
 import { atom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
-import { encryptedStorage } from "../utils";
+import { encryptedStorage } from "../utils/security";
+import { FacilityInterface } from "../utils/search";
 
-
-// 좌측 상단 메뉴 아이콘 클릭 시 열리는 Drawer 상태 관리 아톰
+// 좌측 상단 메뉴 아이콘 클릭 시 열리는 Drawer 상태
 export const isDrawerOpenAtom = atom(false);
 
-// 우측 상단 검색 아이콘 클릭 시 열리는 Drawer 상태 관리 아톰
+// 우측 상단 검색 아이콘 클릭 시 열리는 Drawer 상태
 export const isSearchDrawerOpenAtom = atom(false);
 
-// 검색 기록 - 암호화된 형태로 localStorage에 저장
+// 검색어 상태
+export const keywordAtom = atom("");
+
+// 검색 결과 상태
+export const searchResultAtom = atom<FacilityInterface[]>([]);
+
+// 검색 기록 상태 (로컬 스토리지)
 export const searchHistoryAtom = atomWithStorage<string[]>(
-  'mokwon-search-history', 
-  [], 
+  "mokwon-guide-search-history",
+  [],
   encryptedStorage
 );
