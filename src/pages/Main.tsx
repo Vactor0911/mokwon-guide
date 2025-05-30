92f0d48589bb8de6742257547890f537db11427e
Vactor0911 <vactor0911@gmail.com>
2025-05-30 14:27:15 +0900

    건물 상세 페이지에서 선택한 호실 초기화 기능 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,8 +1,18 @@
 import { Stack } from "@mui/material";
 import BuildingDetailDrawer from "../components/BuildingDetailDrawer";
 import MapViewer from "../components/MapViewer";
+import { useSetAtom } from "jotai";
+import { selectedFacilityAtom } from "../states";
+import { useEffect } from "react";
 
 const Main = () => {
+  const setSelectedFacility = useSetAtom(selectedFacilityAtom); // 선택된 시설 상태
+
+  // 선택된 시설 초기화
+  useEffect(() => {
+    setSelectedFacility(null); 
+  }, [setSelectedFacility]);
+
   return (
     <>
       {/* 지도 뷰어 */}
