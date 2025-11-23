d3cbcf7c66e1f58c27f5b95b7bf1396e140b16ee
Vactor0911 <vactor0911@gmail.com>
2025-11-23 14:18:28 +0900

    시설물 마커 색상 및 아이콘 반환 함수 분리

--- src/components/markers/PlaceMarker.tsx
+++ src/components/markers/PlaceMarker.tsx
@@ -1,9 +1,6 @@
-import { useCallback, useMemo } from "react";
+import { useCallback } from "react";
 import IconMarker from "./IconMarker";
-import LocalCafeRoundedIcon from "@mui/icons-material/LocalCafeRounded";
-import RestaurantRoundedIcon from "@mui/icons-material/RestaurantRounded";
-import StorefrontRoundedIcon from "@mui/icons-material/StorefrontRounded";
-import StoreRoundedIcon from "@mui/icons-material/StoreRounded";
+import { getMarkerColor, getMarkerIcon } from "../../utils/marker";
 
 interface PlaceMarkerProps {
   position: number[];
@@ -13,34 +10,6 @@ interface PlaceMarkerProps {
 const PlaceMarker = (props: PlaceMarkerProps) => {
   const { position, category } = props;
 
-  // 색상
-  const color = useMemo(() => {
-    switch (category) {
-      case "편의점":
-        return "#FFBA01";
-      case "카페":
-        return "#FE8C52";
-      case "식당":
-        return "#FE8C52";
-      default:
-        return "#D971DB";
-    }
-  }, [category]);
-
-  // 아이콘
-  const icon = useMemo(() => {
-    switch (category) {
-      case "편의점":
-        return StorefrontRoundedIcon;
-      case "카페":
-        return LocalCafeRoundedIcon;
-      case "식당":
-        return RestaurantRoundedIcon;
-      default:
-        return StoreRoundedIcon;
-    }
-  }, [category]);
-
   // 건물 마커 클릭
   const handleMarkerClick = useCallback(() => {
     // TODO: 장소 마커 클릭 시 동작 구현
@@ -49,8 +18,8 @@ const PlaceMarker = (props: PlaceMarkerProps) => {
   return (
     <IconMarker
       position={position}
-      color={color}
-      Icon={icon}
+      color={getMarkerColor(category)}
+      Icon={getMarkerIcon(category)}
       onClick={handleMarkerClick}
     />
   );
